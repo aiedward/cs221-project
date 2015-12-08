178a5b65e8ddb201b6a6421ffc0710640e51696d
Austin <aray@cs.stanford.edu>
2015-12-08 15:56:40 -0800

    Organized our repo into binary files, library files, resource files, and backup files.

---
+++ bin/processJsonRecipes.py
@@ -0,0 +1,14 @@
+import collections, itertools, copy, Queue
+import numpy, scipy, math, random
+import os, sys
+import tokenize, re, string
+
+RESOURCES_DIRECTORY = "./res"
+
+# Main Program */
+def main(argv):
+	
+
+# If called from command line, call the main() function
+if __name__ == "__main__":
+    main(sys.argv)
